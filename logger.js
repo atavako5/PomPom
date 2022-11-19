6017bf7c7d68eaac11ebc2c009db7e1cdea6a7d0
Ali Tavakoli <atavako5@uwo.ca>
2022-11-19 14:10:15 -0500

    added log levels

--- logger.js
+++ logger.js
@@ -1,62 +1,84 @@
-
-const pino = require('pino')
-const pretty = require('pino-pretty')
+const pino = require("pino");
+const pretty = require("pino-pretty");
+const dotenv = require("dotenv");
+dotenv.config();
 
 class Logger {
+  constructor() {
+    this.logger = pino({
+      transport: {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+        },
+      },
+    });
+    this.referenceLog = null;
+  }
 
-    constructor() {
-        this.logger = pino({
-            transport: {
-              target: 'pino-pretty',
-              options: {
-                colorize: true,
-                translateTime: "SYS:standard"
-              }
-            },
-          })
-        this.referenceLog = null
-    }
+  fatal(message) {
+    if (
+      process.env.LOGLEVEL === "TRACE" ||
+      process.env.LOGLEVEL === "DEBUG" ||
+      process.env.LOGLEVEL === "INFO" ||
+      process.env.LOGLEVEL === "WARN" ||
+      process.env.LOGLEVEL === "ERROR" ||
+      process.env.LOGLEVEL === "FATAL"
+    )
+      this.logger.fatal(message);
+  }
 
-    fatal(message) {
+  error(message) {
+    if (
+      process.env.LOGLEVEL === "TRACE" ||
+      process.env.LOGLEVEL === "DEBUG" ||
+      process.env.LOGLEVEL === "INFO" ||
+      process.env.LOGLEVEL === "WARN" ||
+      process.env.LOGLEVEL === "ERROR"
+    )
+      this.logger.error(message);
+  }
 
-        this.logger.fatal(message)
-    }
-    
-    error(message) {
+  warn(message) {
+    if (
+      process.env.LOGLEVEL === "TRACE" ||
+      process.env.LOGLEVEL === "DEBUG" ||
+      process.env.LOGLEVEL === "INFO" ||
+      process.env.LOGLEVEL === "WARN"
+    )
+      this.logger.warn(message);
+  }
 
-        this.logger.error(message)
+  info(message) {
+    if (
+      process.env.LOGLEVEL === "TRACE" ||
+      process.env.LOGLEVEL === "DEBUG" ||
+      process.env.LOGLEVEL === "INFO"
+    )
+      this.logger.info(message);
+  }
 
-    }
+  debug(message) {
+    if (process.env.LOGLEVEL === "TRACE" || process.env.LOGLEVEL === "DEBUG")
+      this.logger.debug(message);
+  }
 
-    warn(message) {
-        this.logger.warn(message)
-    }   
-
-    info(message) {
-        this.logger.info(message)
-    } 
-
-    debug(message) {
-        this.logger.debug(message)
-    }
-
-    trace(message) {
-        this.logger.trace(message)
-    }         
+  trace(message) {
+    if (process.env.LOGLEVEL === "TRACE") this.logger.trace(message);
+  }
 }
 
 class Singleton {
-
-    constructor() {
-        if (!Singleton.instance) {
-            Singleton.instance = new Logger();
-        }
+  constructor() {
+    if (!Singleton.instance) {
+      Singleton.instance = new Logger();
     }
-  
-    getInstance() {
-        return Singleton.instance;
-    }
-  
   }
 
-module.exports = Singleton
+  getInstance() {
+    return Singleton.instance;
+  }
+}
+
+module.exports = Singleton;
